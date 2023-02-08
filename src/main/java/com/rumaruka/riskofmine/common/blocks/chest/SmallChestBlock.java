d16c6315f40a5d4f597106e5081a59d647a7d786
Rumaruka <rumarukakunnya@gmail.com>
2023-02-08 22:08:32 +0500

    Better if else :D

--- src/main/java/com/rumaruka/riskofmine/common/blocks/chest/SmallChestBlock.java
+++ src/main/java/com/rumaruka/riskofmine/common/blocks/chest/SmallChestBlock.java
@@ -3,10 +3,14 @@ package com.rumaruka.riskofmine.common.blocks.chest;
 import com.rumaruka.riskofmine.api.Chest;
 import com.rumaruka.riskofmine.api.Types;
 import com.rumaruka.riskofmine.common.cap.Money;
+import com.rumaruka.riskofmine.common.config.ROMConfig;
 import com.rumaruka.riskofmine.common.tiles.chest.GenericChestTE;
 import com.rumaruka.riskofmine.common.tiles.chest.SmallChestTE;
+import com.rumaruka.riskofmine.init.ROMSounds;
 import com.rumaruka.riskofmine.init.ROMTiles;
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.stats.Stats;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
@@ -34,15 +38,20 @@ public class SmallChestBlock extends GenericChestBlock{
         else {
            BlockEntity blockEntity= level.getBlockEntity(blockPos);
            if (blockEntity instanceof GenericChestTE){
-               player.openMenu((GenericChestTE) blockEntity);
-                      player.awardStat(Stats.OPEN_CHEST);
-//               if (money!=null){
-//                   if(money.money.get()>=25){
-//                       money.removeMoney(25);
-//                       money.detectAndSendChanges();
-//                       PiglinAi.angerNearbyPiglins(player, true);
-//                   }
-//               }
+               if(money != null && money.getCurrentMoney()>= ROMConfig.GENERAL.priceSmallChest.get()){
+                   money.consumeMoney(player, ROMConfig.GENERAL.priceSmallChest.get());
+                   money.detectAndSendChanges();
+                   player.openMenu((GenericChestTE) blockEntity);
+                   player.awardStat(Stats.OPEN_CHEST);
+                   PiglinAi.angerNearbyPiglins(player, true);
+               }else if (money != null && money.getCurrentMoney() < ROMConfig.GENERAL.priceSmallChest.get()) {
+                   level.playSound(null, blockPos, ROMSounds.ROM_CHEST_NOT_MONEY.get(), SoundSource.BLOCKS, 2.0F, 1.0F);
+                   player.displayClientMessage(Component.translatable("riskofmine.not_money"), true);
+
+               }
+
+
+
            }
         }
         return InteractionResult.CONSUME;
