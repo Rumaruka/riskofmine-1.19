f8eb1e3e2f752f89342f08eaed7b8231a1da30d5
Rumaruka <rumarukakunnya@gmail.com>
2023-04-13 21:45:47 +0500

    Write new realtime mechanics

--- src/main/java/com/rumaruka/riskofmine/common/events/TimerEvents.java
+++ src/main/java/com/rumaruka/riskofmine/common/events/TimerEvents.java
@@ -2,48 +2,32 @@ package com.rumaruka.riskofmine.common.events;
 
 import com.rumaruka.riskofmine.common.cap.Timer;
 import com.rumaruka.riskofmine.init.ROMEffects;
+import com.rumaruka.riskofmine.utils.ROMUtils;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
+import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.event.entity.living.LivingEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber
 public class TimerEvents {
+    private static long time;
 
 //    @SubscribeEvent
-//    public static void addTimer(LivingEvent.LivingTickEvent event) {
-//        LivingEntity entity = event.getEntity();
-//        if (entity instanceof Player player) {
-//            Level level = player.getLevel();
-//            if (!level.isClientSide()) {
-//                Timer timer = Timer.of(player);
-//                if (player.tickCount % 2 == 0) {
-//                    if (timer != null) {
-//                        timer.addTime(0);
-//                        timer.detectAndSendChanges();
-//                    }
-//                }
+//    public static void onServerTickEvent(TickEvent.ServerTickEvent event){
+//        if (15>0){
+//            if (time==0L){
+//                time = System.currentTimeMillis();
 //            }
-//        }
-//    }
-//    @SubscribeEvent
-//    public static void giveElites(LivingEvent.LivingTickEvent event) {
-//        LivingEntity entity = event.getEntity();
-//
-//        Level level = entity.getLevel();
-//        if (!level.isClientSide()) {
-//            if (entity instanceof Player player) {
-//                Timer timer = Timer.of(player);
-//                if (timer != null) {
-//                    if (  timer.getCurrentTime() > 100){
-//
-//                        entity.addEffect(new MobEffectInstance(ROMEffects.OVERLOADING.get(),1,1,false,false));
-//                        timer.detectAndSendChanges();
-//                    }
-//
+//            long minute = 15 * 60L * 1000L;
+//            if (System.currentTimeMillis() - time > minute){
+//                Timer timer = Timer.of(ROMUtils.getPlayer());
+//                if (timer!=null){
+//                    timer.addTime(1);
+//                    timer.detectAndSendChanges();
 //                }
 //            }
 //        }
